6378b9f234e09ed49167b7b89c4c237db9362dbb
Shruti Pahadiya <15.shruti@gmail.com>
2020-06-22 18:55:42 -0400

    stretch

--- recipe.js
+++ recipe.js
@@ -3,5 +3,29 @@ const Sequelize = require('sequelize');
 
 //YOUR CODE GOES HERE
 
+const Receipe = db.define('Receipe',{
+    name:{
+        type:Sequelize.STRING,
+        defaultValue:'cereal',
+        validate: {
+            notEmpty: true,
+          }
+    },
+    cooktime:{
+        min:1,
+        max:60,
+    },
+    vegan:{
+        type:Sequelize.BOOLEAN,
+    },
+    foodgroup:{
+        type:Sequelize.ENUM,
+        values:['vegetable','meat','dairy','grain','fruit','coffee'],
+    }
+});
+
+
+Receipe.create = Receipe.create
+
 //--------------------
 module.exports = Recipe;
